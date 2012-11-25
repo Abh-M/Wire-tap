ffa0bdeed4a91987c05582340b369ae98c07edd8
AbhineetM <mygitrepo@gmail.com>
2012-11-25 13:06:30 -0500

    Initial Commit

---
+++ Wire tap/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Wire tap
+//
+//  Created by Abhineet on 25/11/12.
+//  Copyright (c) 2012 Abhineet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
